f21b6b1c8775c8b3120fca0d96bf1729a827ab16
Tuerlings <t.r.s.tuerlings@student.tue.nl>
2020-10-19 17:41:35 +0200

    nu wel met logistic regression?

--- code/cad_project.py
+++ code/cad_project.py
@@ -98,15 +98,15 @@ def nuclei_measurement():
 
 def nuclei_classification():
     ## dataset preparation
-    fn = '../data/nuclei_data_classification.mat'
+    fn = 'data/nuclei_data_classification.mat'
     mat = scipy.io.loadmat(fn)
 
     test_images = mat["test_images"] # (24, 24, 3, 20730)
     test_y = mat["test_y"] # (20730, 1)
     training_images = mat["training_images"] # (24, 24, 3, 14607)
     training_y = mat["training_y"] # (14607, 1)
-    validation_images = mat["validation_images"] # (24, 24, 3, 7303)
-    validation_y = mat["validation_y"] # (7303, 1)
+    validation_images = mat["validation_images"] # (24, 24, 3, 14607)
+    validation_y = mat["validation_y"] # (14607, 1)
 
     ## dataset preparation
     imageSize = training_images.shape
@@ -137,140 +137,18 @@ def nuclei_classification():
     # (batch_size) and number of iterations (num_iterations), as well as
     # initial values for the model parameters (Theta) that will result in
     # fast training of an accurate model for this classification problem.
-    mu = 0.01
-    batch_size = 500 
-    num_iterations = 2000 
+    mu = 0.0001
+    batch_size = 100 
+    num_iterations = 4000
     
     # Theta = initialize, so begin with zeros
     # furthermore it is a vector, with as many columns as the trainingset
     # note that there is the addones
     # so, shape of the vector is: the amount of columns of the trainingset
-    # plus 1 by 1. (nr columns+1,1) 
+    # plus 1 as the amount of rows by 1 column. 
     
-    nr_columns=np.shape(training_x)[1] #1728
-    
-    shapex=sum([nr_columns,1]) #1729
-    
-    #using different Theta inputs
-    #Theta = np.zeros(shapex,1) #zeros
-    Theta = np.random.rand(shapex, 1)#random between 0 and 1
-    
-    #-------------------------------------------------------------------#
-
-    xx = np.arange(num_iterations)
-    loss = np.empty(*xx.shape)
-    loss[:] = np.nan
-    validation_loss = np.empty(*xx.shape)
-    validation_loss[:] = np.nan
-    g = np.empty(*xx.shape)
-    g[:] = np.nan
-
-    fig = plt.figure(figsize=(8,8))
-    ax2 = fig.add_subplot(111)
-    ax2.set_xlabel('Iteration')
-    ax2.set_ylabel('Loss (average per sample)')
-    ax2.set_title('mu = '+str(mu))
-    h1, = ax2.plot(xx, loss, linewidth=2) #'Color', [0.0 0.2 0.6],
-    h2, = ax2.plot(xx, validation_loss, linewidth=2) #'Color', [0.8 0.2 0.8],
-    ax2.set_ylim(0, 0.7)
-    ax2.set_xlim(0, num_iterations)
-    ax2.grid()
-
-    text_str2 = 'iter.: {}, loss: {:.3f}, val. loss: {:.3f}'.format(0, 0, 0)
-    txt2 = ax2.text(0.3, 0.95, text_str2, bbox={'facecolor': 'white', 'alpha': 1, 'pad': 10}, transform=ax2.transAxes)
-
-    for k in np.arange(num_iterations):
-        # pick a batch at random
-        idx = np.random.randint(training_x.shape[0], size=batch_size)
-
-        training_x_ones = util.addones(training_x[idx,:])
-        validation_x_ones = util.addones(validation_x)
-
-        # the loss function for this particular batch
-        loss_fun = lambda Theta: cad.lr_nll(training_x_ones, training_y[idx], Theta)
-
-        # gradient descent
-        # instead of the numerical gradient, we compute the gradient with
-        # the analytical expression, which is much faster
-        Theta_new = Theta - mu*cad.lr_agrad(training_x_ones, training_y[idx], Theta).T
-
-        loss[k] = loss_fun(Theta_new)/batch_size
-        validation_loss[k] = cad.lr_nll(validation_x_ones, validation_y, Theta_new)/validation_x.shape[0]
-
-        # visualize the training
-        h1.set_ydata(loss)
-        h2.set_ydata(validation_loss)
-        text_str2 = 'iter.: {}, loss: {:.3f}, val. loss={:.3f} '.format(k, loss[k], validation_loss[k])
-        txt2.set_text(text_str2)
-
-        Theta = None
-        Theta = np.array(Theta_new)
-        Theta_new = None
-        tmp = None
-
-        display(fig)
-        clear_output(wait = True)
-        plt.pause(.005)
-
-
-def nuclei_classification2():
-    ## dataset preparation
-    fn = '../data/nuclei_data_classification.mat'
-    mat = scipy.io.loadmat(fn)
-
-    test_images = mat["test_images"] # (24, 24, 3, 20730)
-    test_y = mat["test_y"] # (20730, 1)
-    training_images = mat["training_images"] # (24, 24, 3, 14607)
-    training_y = mat["training_y"] # (14607, 1)
-    validation_images = mat["training_images"] # (24, 24, 3, 14607)
-    validation_y = mat["training_y"] # (14607, 1)
-
-    ## dataset preparation
-    imageSize = training_images.shape
-    # every pixel is a feature so the number of features is:
-    # height x width x color channels
-    numFeatures = imageSize[0]*imageSize[1]*imageSize[2]
-    training_x = training_images.reshape(numFeatures, training_images.shape[3]).T.astype(float)
-    validation_x = validation_images.reshape(numFeatures, validation_images.shape[3]).T.astype(float)
-    test_x = test_images.reshape(numFeatures, test_images.shape[3]).T.astype(float)
-
-    # the training will progress much better if we
-    # normalize the features
-    meanTrain = np.mean(training_x, axis=0).reshape(1,-1)
-    stdTrain = np.std(training_x, axis=0).reshape(1,-1)
-
-    training_x = training_x - np.tile(meanTrain, (training_x.shape[0], 1))
-    training_x = training_x / np.tile(stdTrain, (training_x.shape[0], 1))
-
-    validation_x = validation_x - np.tile(meanTrain, (validation_x.shape[0], 1))
-    validation_x = validation_x / np.tile(stdTrain, (validation_x.shape[0], 1))
-
-    test_x = test_x - np.tile(meanTrain, (test_x.shape[0], 1))
-    test_x = test_x / np.tile(stdTrain, (test_x.shape[0], 1))
-
-    ## training linear regression model
-    #-------------------------------------------------------------------#
-    # TODO: Select values for the learning rate (mu), batch size
-    # (batch_size) and number of iterations (num_iterations), as well as
-    # initial values for the model parameters (Theta) that will result in
-    # fast training of an accurate model for this classification problem.
-    mu = 0.01
-    batch_size = 500 
-    num_iterations = 2000 
-    
-    # Theta = initialize, so begin with zeros
-    # furthermore it is a vector, with as many columns as the trainingset
-    # note that there is the addones
-    # so, shape of the vector is: the amount of columns of the trainingset
-    # plus 1 by 1. (nr columns+1,1) 
-    
-    nr_columns=np.shape(training_x)[1] #1728
-    
-    shapex=sum([nr_columns,1]) #1729
-    
-    #using different Theta inputs
-    #Theta = np.zeros(shapex,1) #zeros
-    Theta = np.random.rand(shapex, 1)#random between 0 and 1
+    #Theta = np.zeros((np.shape(training_x)[1])+1,1)
+    Theta = 0.0001*np.ones((training_x.shape[1]+1,1))
     
     #-------------------------------------------------------------------#
 
